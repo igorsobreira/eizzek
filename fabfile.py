b10a9fcdc7e81b0ce94baa3ffc86505168c9230b
Igor Sobreira <igor@igorsobreira.com>
2010-07-03 00:59:28 -0300

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from os.path import join
+import os.path
 from fabric.api import *
 
 import config
@@ -6,30 +6,48 @@ import config
 env.hosts = [config.SSH_HOST]  # format: username@host:port
 
 VIRTUALENV_DIR = '/home/igor/eizzek_env'
-EIZZEK_DIR = join(VIRTUALENV_DIR, 'eizzek')
+EIZZEK_DIR = os.path.join(VIRTUALENV_DIR, 'eizzek')
 
-python = join(VIRTUALENV_DIR, 'bin', 'python')
+python = os.path.join(VIRTUALENV_DIR, 'bin', 'python')
+twistd = os.path.join(VIRTUALENV_DIR, 'bin', 'twistd')
 
 
-def update_sleekxmpp():
-    with cd(join(VIRTUALENV_DIR, 'SleekXMPP')):
-        run('git pull')
-        run('%s setup.py install' % python)
+def update_deps(*deps):
+    for dep in deps:
+        print ' - Updating %s' % dep
+        with cd( os.path.join(VIRTUALENV_DIR, dep) ):
+            run('git pull')
+            run('%s setup.py install' % python)
 
-def update():
+
+def update(all=False):
+    ''' Update the project. Use :all to update all git depedencies '''
+    if all:
+        update_deps('wokkel')
     with cd(EIZZEK_DIR):
+        print ' - Updating eizzek'
         run('git pull')
-        send_config()
+    send_config()
 
 
-# def start():
-#     with cd(EIZZEK_DIR):
-#         out = run('screen -dm "%s main.py"' % python)
-#         print out
+def start():
+    ''' Start bot service '''
+    with cd(EIZZEK_DIR):
+        out = run('%s -y twistd.tac' % twistd)
+
+
+def stop(force=False):
+    ''' Stop the bot. Use :force to kill -9. Default is -15 ''' 
+    with cd(EIZZEK_DIR):
+        if 'twistd.pid' not in run('ls'):
+            print ' - Not running'
+            return
+        pid = run('cat twistd.pid')
+        force = '-9' if force else '-15'
+        run( 'kill %s %s' % (force, pid) )
 
 
 def send_config():
-    config = local('cat config.py')
-    with cd(EIZZEK_DIR):
-        run('echo "%s" > config.py' % config)
+    ''' Send the local config.py to the server '''
+    put('config.py', os.path.join(EIZZEK_DIR, 'config.py'))
 
