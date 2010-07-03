654dd761ef93fffb287edaa0b7ddffe9ad6f46e8
Igor Sobreira <igor@igorsobreira.com>
2010-07-02 21:23:09 -0300

    initial

---
+++ README.md
@@ -0,0 +1,16 @@
+Eizzek Bot
+==========
+
+Eizzek is a simple g-talk bot.
+
+It's currently built using [SleekXMPP](http://github.com/fritzy/SleekXMPP).
+
+config.py
+---------
+
+You need to create a config.py file with some variables:
+
+    JID = 'username@gmail.com/fun'
+    PASSWORD = 'secret'
+    SERVER = 'talk.google.com'
+    PORT = 5222
---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+class Eizzek(object):
+    
+    def __init__(self, jid, password):
+        self.xmpp = sleekxmpp.ClientXMPP(jid, password)    
+        self._connect_handlers()
+    
+    def _connect_handlers(self):
+        self.xmpp.add_event_handler("session_start", self.handle_session_start) 
+        self.xmpp.add_event_handler("message", self.handle_message)
+    
+    def run(self, address=()):
+        self.xmpp.connect(address)
+        self.xmpp.process(threaded=False)
+    
+    def handle_session_start(self, event): 
+        self.xmpp.sendPresence(pstatus = "Hi, I'm a bot")
+    
+    def handle_message(self, message): 
+        self.xmpp.sendMessage(message['from'].jid, message["body"])
+    
+
+
+def main():
+    import config
+    bot = EchoBot(config.ACCOUNT, config.PASSWORD)
+    bot.run((config.SERVER, config.PORT))
+
+
+if __name__ == '__main__':
+    main()
