d6af9c12ac4e1e8d108c5a1bb56398ee46ed652f
Igor Sobreira <igor@igorsobreira.com>
2010-09-18 22:06:27 -0300

    class to register session plugins

--- eizzek/lib/registry.py
+++ eizzek/lib/registry.py
@@ -1,31 +1,55 @@
+from abc import ABCMeta, abstractmethod
 
-class PluginRegistry(object):
-    '''
-    Register Eizzek plugins.
-    '''
-    
-    _plugins = {}
-    
-    def register(self, name, regex, callable_obj):
-        if name in self.plugins:
-            return
-        self._plugins[name] = (regex, callable_obj)
-    
-    def unregister(self, name_or_callable):
-        if hasattr(name_or_callable, '__name__'):
-            name_or_callable = name_or_callable.__name__
-        self._plugins.pop(name_or_callable, None)
+class BaseRegistry(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        self._plugins = {}
     
     @property
     def plugins(self):
         return self._plugins
     
+    def clear(self):
+        self._plugins = {}
     
-    @classmethod
-    def clear(cls):
-        cls._plugins = {}
+    @abstractmethod
+    def register(self):
+        pass
     
+    @abstractmethod
+    def unregister(self):
+        pass
 
+
+class PluginRegistry(BaseRegistry):
+    '''
+    Register simple plugins.
+    '''    
+    
+    def register(self, name, regex, callable_obj):
+        if name in self.plugins:
+            return
+        self._plugins[name] = (regex, callable_obj)
+
+    def unregister(self, name_or_callable):
+        name = getattr(name_or_callable, '__name__', name_or_callable)
+        self._plugins.pop(name, None)
+
+class SessionPluginRegistry(BaseRegistry):
+    '''
+    Register session plugins
+    '''
+
+    def register(self, klass):
+        if klass.name in self.plugins:
+            return
+        self._plugins[klass.name] = (klass.regex, klass)
+
+    def unregister(self, name_or_klass):
+        name = getattr(name_or_klass, 'name', name_or_klass)
+        self._plugins.pop(name, None)
 
 
 registry = PluginRegistry()
+session_registry = SessionPluginRegistry()
--- tests/unit/test_register.py
+++ tests/unit/test_register.py
@@ -1,14 +1,13 @@
 from unittest import TestCase
 
-from eizzek.lib.registry import PluginRegistry
+from eizzek.lib.registry import PluginRegistry, SessionPluginRegistry
 
 class PluginRegistryTest(TestCase):
     
     def setUp(self):
-        PluginRegistry.clear()
         self.registry = PluginRegistry()
         
-        def ping(): 
+        def ping():
             return ''
         self.ping = ping
         self.regex = r'^ping (.+)$'
@@ -33,7 +32,7 @@ class PluginRegistryTest(TestCase):
         assert self.registry.plugins['ping'] == (self.regex, self.ping)
     
     
-    def test_unregister_plugin(self):
+    def test_unregister_plugin_by_name(self):
         
         assert len(self.registry.plugins) == 0
         
@@ -44,12 +43,14 @@ class PluginRegistryTest(TestCase):
         self.registry.unregister('ping')     # by name
         
         assert len(self.registry.plugins) == 0
-        
+    
+    def test_unregister_plugin_by_callable(self):
+
         self.registry.register(self.ping.__name__, self.regex, self.ping)
         
         assert len(self.registry.plugins) == 1
         
-        self.registry.unregister(self.ping)     # by callable, with __name__ attribute
+        self.registry.unregister(self.ping)     # by callable, using __name__ attribute
         
         assert len(self.registry.plugins) == 0
     
@@ -62,4 +63,70 @@ class PluginRegistryTest(TestCase):
         self.registry.clear()
         
         assert len(self.registry.plugins) == 0
-    
+
+
+class SessionPluginRegistryTest(TestCase):
+
+    def setUp(self):
+        self.session_registry = SessionPluginRegistry()
+         
+        class TranslateSessionPlugin(object):
+            name = 'translate'
+            regex = r'^translate (?P<from>\w+) (?P<to>\w+)$'
+        
+        self.translate = TranslateSessionPlugin
+
+
+    def test_register_plugin(self):
+        
+        assert 0 == len(self.session_registry.plugins)
+        
+        self.session_registry.register(self.translate)
+
+        assert 1 == len(self.session_registry.plugins)
+        assert (self.translate.regex, self.translate) == self.session_registry.plugins[self.translate.name]
+        
+        class OtherTranslatePlugin(object):
+            name = 'translate'
+            regex = r'*'
+
+        # ignore duplicates
+        self.session_registry.register(OtherTranslatePlugin)
+        
+        assert 1 == len(self.session_registry.plugins)
+
+        assert (self.translate.regex, self.translate) == self.session_registry.plugins[self.translate.name]
+
+
+    def test_unregister_plugin_by_name(self):
+        
+        assert 0 == len(self.session_registry.plugins)
+        
+        self.session_registry.register(self.translate)
+        
+        assert 1 == len(self.session_registry.plugins)
+        
+        self.session_registry.unregister(self.translate.name)
+        
+        assert 0 == len(self.session_registry.plugins)
+
+
+    def test_unregister_plugin_by_callable(self):
+        
+        self.session_registry.register(self.translate)
+        
+        assert 1 == len(self.session_registry.plugins)
+        
+        self.session_registry.unregister(self.translate)     # by callable, using __name__ attribute
+        
+        assert 0 == len(self.session_registry.plugins)
+
+
+    def test_clear(self):
+        self.session_registry.register(self.translate)
+        
+        assert 1 == len(self.session_registry.plugins)
+        
+        self.session_registry.clear()
+        
+        assert 0 == len(self.session_registry.plugins)
